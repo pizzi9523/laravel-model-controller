9873fd1b5814aca2fee7926fddc7dcf13cb7d283
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-11 15:00:02 +0100

    Return view home by PageController

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // return view home
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome in Home</h1>
+    
+@endsection
